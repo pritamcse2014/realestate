dfc15ef74a94ccdab2ac5c84c2588e4b791a2465
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-10-30 22:35:43 +0600

    Refreach-Admin-Template

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -9,7 +9,7 @@
         <meta name="author" content="NobleUI" />
         <meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web" />
 
-        <title>NobleUI - HTML Bootstrap 5 Admin Dashboard Template</title>
+        <title>NobleUI - Admin Dashboard</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com" />
