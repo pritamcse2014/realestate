c59c00a6253e3556a937b96e699a3be83a914ce5
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-10-30 22:08:11 +0600

    Customize-Login-Form

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -19,6 +19,12 @@ class AdminController extends Controller
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/login');
+        return redirect('admin/login');
+    }
+
+    public function AdminLogin() {
+        // echo "Login";
+        // die();
+        return view('admin.admin_login');
     }
 }
