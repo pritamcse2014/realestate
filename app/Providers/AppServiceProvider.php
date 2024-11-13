fe4fa3d1ea369f9131109119bb4cf418adf0cdfc
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-11-13 15:41:28 +0600

    How-To-Set-Dynamic-SMTP-Details-In-Laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\SMTP;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,26 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+
+        $mailSetting = SMTP::find(1);
+        if ($mailSetting) {
+            $data = [
+                "driver" => $mailSetting->mail_mailer,
+                "host" => $mailSetting->mail_host,
+                "port" => $mailSetting->mail_port,
+                "username" => $mailSetting->mail_username,
+                "password" => $mailSetting->mail_password,
+                "encryption" => $mailSetting->mail_encryption,
+                "from" => [
+                    "address" => $mailSetting->mail_from_address,
+                    "name" => $mailSetting->app_name,
+                ]
+            ];
+            $AppName = [
+                "name" => $mailSetting->app_name,
+            ];
+            Config::set("app.name", $AppName["name"]);
+            Config::set("mail", $data);
+        }
     }
 }
