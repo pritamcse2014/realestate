f46639d8fecdd0faef1f9f1f7b481e5f43fc005f
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-11-21 11:46:18 +0600

    Eloquent-Multiple-Tables-Join-With-Filter-In-Laravel

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Request;
 
 class City extends Model
 {
@@ -12,10 +13,21 @@ class City extends Model
     protected $table = 'city';
 
     static public function getDetails() {
-        return self::select('city.*', 'countries.country_name', 'state.state_name')
+        $return = self::select('city.*', 'countries.country_name', 'state.state_name')
                 ->join('countries', 'countries.id', '=', 'city.countries_id')
                 ->join('state', 'state.id', '=', 'city.state_id')
-                ->orderBy('city.id', 'desc')
-                ->paginate(10);
+                ->orderBy('city.id', 'desc');
+                if (!empty(Request::get('id'))) {
+                    $return = $return->where('city.id', '=', Request::get('id'));
+                } if (!empty(Request::get('country_name'))) {
+                    $return = $return->where('countries.country_name', 'like', '%' . Request::get('country_name') . '%');
+                } if (!empty(Request::get('state_name'))) {
+                    $return = $return->where('state.state_name', 'like', '%' . Request::get('state_name') . '%');
+                } if (!empty(Request::get('city_name'))) {
+                    $return = $return->where('city.city_name', 'like', '%' . Request::get('city_name') . '%');
+                }
+        $return = $return->paginate(10);
+        return $return;
+            
     }
 }
--- resources/views/admin/city/list.blade.php
+++ resources/views/admin/city/list.blade.php
@@ -13,6 +13,58 @@
         <div class="col-lg-12 stretch-card">
             <div class="card">
                 <div class="card-body">
+                    <h6 class="card-title">Search City</h6>
+                    <form method="GET" action="">
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <div class="mb-3">
+                                    <label class="form-label">Enter Your ID</label>
+                                    <input class="form-control" type="text" name="id" id="id" value="{{ Request()->id }}" placeholder="Enter Your ID" />
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="mb-3">
+                                    <label class="form-label">Enter Your Country Name</label>
+                                    <input class="form-control" type="text" name="country_name" id="country_name" value="{{ Request()->country_name }}" placeholder="Enter Your Country Name" />
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="mb-3">
+                                    <label class="form-label">Enter Your State Name</label>
+                                    <input class="form-control" type="text" name="state_name" id="state_name" value="{{ Request()->state_name }}" placeholder="Enter Your State Name" />
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="mb-3">
+                                    <label class="form-label">Enter Your City Name</label>
+                                    <input class="form-control" type="text" name="city_name" id="city_name" value="{{ Request()->city_name }}" placeholder="Enter Your City Name" />
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="mb-3">
+                                    <label class="form-label">Enter Your Created At</label>
+                                    <input class="form-control" type="date" name="created_at" id="created_at" value="{{ Request()->created_at }}" placeholder="Enter Your Created At" />
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="mb-3">
+                                    <label class="form-label">Enter Your Updated At</label>
+                                    <input class="form-control" type="date" name="updated_at" id="updated_at" value="{{ Request()->updated_at }}" placeholder="Enter Your Updated At" />
+                                </div>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary me-1" type="submit">Search</button>
+                        <a class="btn btn-danger ms-1" href="{{ url('admin/city') }}">Reset</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br />
+    <div class="row">
+        <div class="col-lg-12 stretch-card">
+            <div class="card">
+                <div class="card-body">
                     <div class="d-flex justify-content-between align-items-center flex-wrap">
                         <h4 class="card-title">City List</h4>
                         <div class="d-flex align-items-center">
