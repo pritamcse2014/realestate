d6fca6db5c8078c5b49dbe6fd86ed4e8765b841c
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-11-22 19:07:25 +0600

    Country-State-City-Join-Table-Search-Data-In-Database-Using-Model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Request;
 
 class Address extends Model
 {
@@ -16,6 +17,15 @@ class Address extends Model
                 ->join('countries', 'countries.id', '=', 'address.countries_id')
                 ->join('state', 'state.id', '=', 'address.state_id')
                 ->join('city', 'city.id', '=', 'address.city_id');
+                if (!empty(Request::get('id'))) {
+                    $return = $return->where('address.id', '=', Request::get('id'));
+                } if (!empty(Request::get('country_name'))) {
+                    $return = $return->where('countries.country_name', 'like', '%' . Request::get('country_name') . '%');
+                } if (!empty(Request::get('state_name'))) {
+                    $return = $return->where('state.state_name', 'like', '%' . Request::get('state_name') . '%');
+                } if (!empty(Request::get('city_name'))) {
+                    $return = $return->where('city.city_name', 'like', '%' . Request::get('city_name') . '%');
+                }
         $return = $return->orderBy('address.id', 'desc')->paginate(10);
         return $return;
     }
