788557f87ccdc81c250a0f358e66a167e5e14945
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-11-27 18:18:06 +0600

    Filter-Based-On-Joined-Table-In-Laravel

--- app/Models/DiscountCode.php
+++ app/Models/DiscountCode.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Request;
 
 class DiscountCode extends Model
 {
@@ -14,6 +15,15 @@ class DiscountCode extends Model
     static public function getDetails() {
         $return = self::select('discount_code.*', 'users.name')
                 ->join('users', 'users.id', '=', 'discount_code.user_id');
+                if (!empty(Request::get('id'))) {
+                    $return = $return->where('discount_code.id', '=', Request::get('id'));
+                } if (!empty(Request::get('name'))) {
+                    $return = $return->where('users.name', 'like', '%' . Request::get('name') . '%');
+                } if (!empty(Request::get('discount_code'))) {
+                    $return = $return->where('discount_code.discount_code', 'like', '%' . Request::get('discount_code') . '%');
+                } if (!empty(Request::get('discount_price'))) {
+                    $return = $return->where('discount_code.discount_price', 'like', '%' . Request::get('discount_price') . '%');
+                }
         $return = $return->orderBy('discount_code.id', 'desc')->paginate(10);
         return $return;
     }
